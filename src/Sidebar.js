ac66f7015980857023c5e3169f073d382b4d72a0
Silas Green <scgreen160@gmail.com>
2022-11-07 19:54:10 -0700

    getting there

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
 
 function Sidebar() {
   return (
@@ -9,6 +13,18 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar_title">PLAYLISTS</strong>
+      <hr />
+
+      <SidebarOption title="Today's Top 20" />
+      <SidebarOption title="Suggested For You" />
+      <SidebarOption title="Rock" />
+      <SidebarOption title="" />
     </div>
   );
 }
